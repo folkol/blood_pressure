407a6428035337463e4cc5d12b4464ecdfc598f7
Matte <mattias.johansson@atex.com>
2015-11-21 23:30:22 +0100

    Matplotlib example. Bell curve + TeX text string

--- plot.py
+++ plot.py
@@ -1,5 +1,17 @@
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
-data=pylab.loadtxt('readings', usecols=(2,3,4))
-pylab.plot(data)
-pylab.show()
+
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(50, 0.022, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
