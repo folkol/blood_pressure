b6e6a7788b82499d1b2e10f607acf257b51407a7
Matte <mattias.johansson@atex.com>
2015-11-22 00:13:59 +0100

    Subplots, sharing axis

--- plot.py
+++ plot.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 # fromfile = np.fromfile('readings.dat', sep=' ', count=-1)
 data = np.loadtxt('readings.dat', skiprows=1, usecols=(2, 3, 4))
 
-plt.plot(data)
+morning = data[::2]
+evening = data[1::2]
+
+ax1 = plt.subplot(121)
+plt.plot(morning)
+plt.subplot(122, sharey=ax1)
+plt.plot(evening)
+
 plt.show()
