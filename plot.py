1f5f809a0dfcd1ee93893715fe2694004d92c845
Matte <mattias.johansson@atex.com>
2015-11-22 01:37:28 +0100

    Minor refactorings

--- plot.py
+++ plot.py
@@ -3,37 +3,37 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt('readings.dat', skiprows=1, usecols=(2, 3))
 
-morning = data[::2]
-evening = data[1::2]
 
+morning = data[::2]
+title = 'Morning'
 ax1 = plt.subplot(121)
 plt.plot(morning)
 plt.grid(True)
-plt.title('Morning')
+plt.title(title)
 sys = morning[:, 0]
 dia = morning[:, 1]
 plt.legend([
     'SYS:\t${(min=%5.d, max=%d, mean=%d)}$' % (sys.min(), sys.max(), sys.mean()),
     'DIA:\t${(min=%d, max=%d, mean=%d)}$' % (dia.min(), dia.max(), dia.mean())
 ], loc=2, fancybox=True)
-xlim = ax1.get_xlim()
-plt.hlines(dia.mean(), xlim[0], xlim[1], linestyles='--')
-plt.hlines(sys.mean(), xlim[0], xlim[1], linestyles='--')
+plt.axhline(sys.mean(), linestyle='--', linewidth=0.5)
+plt.axhline(dia.mean(), linestyle='--', linewidth=0.5)
 plt.xticks([])
 
+evening = data[1::2]
+title = 'Evening'
 plt.subplot(122, sharey=ax1)
 plt.plot(evening)
 plt.grid(True)
-plt.title('Evening')
+plt.title(title)
 sys = evening[:, 0]
 dia = evening[:, 1]
 plt.legend([
     'SYS:\t${(min=%5.d, max=%d, mean=%d)}$' % (sys.min(), sys.max(), sys.mean()),
     'DIA:\t${(min=%d, max=%d, mean=%d)}$' % (dia.min(), dia.max(), dia.mean())
 ], loc=2, fancybox=True)
+plt.axhline(sys.mean(), linestyle='--', linewidth=0.5)
+plt.axhline(dia.mean(), linestyle='--', linewidth=0.5)
 plt.xticks([])
-xlim = ax1.get_xlim()
-plt.hlines(sys.mean(), xlim[0], xlim[1], linestyles='--')
-plt.hlines(dia.mean(), xlim[0], xlim[1], linestyles='--')
 
 plt.show()
