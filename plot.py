27f2a3616611240d2475f129b7dbd96a32383f7d
Matte <mattias.johansson@atex.com>
2015-11-22 02:21:31 +0100

    More reformatting...

--- plot.py
+++ plot.py
@@ -6,15 +6,16 @@ def plot(title, data):
     plt.plot(data)
     plt.grid(True)
     plt.title(title, fontsize=20)
+
     sys = data[:, 0]
     dia = data[:, 1]
     plt.legend([
         '%s:\t${(min=%d, max=%d, mean=%d)}$' % ('SYS', sys.min(), sys.max(), sys.mean()),
         '%s:\t${(min=%d, max=%d, mean=%d)}$' % ('DIA', dia.min(), dia.max(), dia.mean())
     ], loc=2, fancybox=True)
+
     plt.axhline(sys.mean(), linestyle='--', linewidth=0.5, color='black')
     plt.axhline(dia.mean(), linestyle='--', linewidth=0.5, color='black')
-    # plt.xticks([])
     plt.gca().axes.xaxis.set_ticklabels([])
 
 
