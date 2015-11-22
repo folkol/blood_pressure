98e0b375db6725982235ad445f7e6bd453db2e6d
Matte <mattias.johansson@atex.com>
2015-11-22 02:20:51 +0100

    Reformatted the legend and made the hlines vblack

--- plot.py
+++ plot.py
@@ -9,11 +9,11 @@ def plot(title, data):
     sys = data[:, 0]
     dia = data[:, 1]
     plt.legend([
-        'SYS:\t${(min=%5.d, max=%d, mean=%d)}$' % (sys.min(), sys.max(), sys.mean()),
-        'DIA:\t${(min=%d, max=%d, mean=%d)}$' % (dia.min(), dia.max(), dia.mean())
+        '%s:\t${(min=%d, max=%d, mean=%d)}$' % ('SYS', sys.min(), sys.max(), sys.mean()),
+        '%s:\t${(min=%d, max=%d, mean=%d)}$' % ('DIA', dia.min(), dia.max(), dia.mean())
     ], loc=2, fancybox=True)
-    plt.axhline(sys.mean(), linestyle='--', linewidth=0.5)
-    plt.axhline(dia.mean(), linestyle='--', linewidth=0.5)
+    plt.axhline(sys.mean(), linestyle='--', linewidth=0.5, color='black')
+    plt.axhline(dia.mean(), linestyle='--', linewidth=0.5, color='black')
     # plt.xticks([])
     plt.gca().axes.xaxis.set_ticklabels([])
 
